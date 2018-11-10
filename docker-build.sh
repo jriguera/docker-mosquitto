45456c9bf6bbdee20a1d3c50013112ba999260a8
Jose Riguera <jriguera@gmail.com>
2018-11-10 17:54:52 +0100

    cosmetic

--- docker-build.sh
+++ docker-build.sh
@@ -5,7 +5,7 @@ NAME="mosquitto"
 
 pushd docker
     $DOCKER build \
-      --build-arg  ARCH=$(dpkg --print-architecture) \
+      --build-arg ARCH=$(dpkg --print-architecture) \
       --build-arg TZ=$(timedatectl  | awk '/Time zone:/{ print $3 }') \
       .  -t $NAME
 popd
