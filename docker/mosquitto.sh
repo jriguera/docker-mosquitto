0179b76b1fd5913d82056bac0337cb536f2aad53
Jose Riguera <jriguera@gmail.com>
2019-11-01 20:16:33 +0100

    improve script

--- docker/mosquitto.sh
+++ docker/mosquitto.sh
@@ -36,28 +36,28 @@ done
 # (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
 #  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
 file_env() {
-	local var="$1"
+	local var="${1}"
 	local def="${2:-}"
 
-	local var_file="${var}_FILE"
+	local fvar="${var}_FILE"
 	local val="${def}"
 
-	if [ "${!var:-}" ] && [ "${!var_file:-}" ]
+	if [ -n "${!var:-}" ] && [ -r "${fvar}" ]
 	then
-		echo >&2 "Warning: both ${var} and ${var_file} are set (${var_file} takes priority)"
+		echo "* Warning: both ${var} and ${fvar} are set, env ${var} takes priority"
 	fi
-	if [ "${!var_file:-}" ]
-	then
-		val="$(< "${!var_file}")"
-	elif [ "${!var:-}" ]
+	if [ -n "${!var:-}" ]
 	then
 		val="${!var}"
+	elif [ -r "${fvar}" ]
+	then
+		val=$(< "${fvar}")
 	fi
 	export "${var}"="${val}"
 }
 
 # allow the container to be started with `--user`
-if [ "$1" == "mosquitto" ] && [ "${HELP}" == "0" ] && [ "$(id -u)" == '0' ]
+if [ "${1}" == "mosquitto" ] && [ "${HELP}" == "0" ] && [ "$(id -u)" == "0" ]
 then
 	chown -R mosquitto:mosquitto "${MQTT_PATH}" "${MQTT_CONFIG}"
 	exec su-exec mosquitto "${BASH_SOURCE}" "$@"
@@ -66,12 +66,12 @@ fi
 # if no configfile is provided, generate one based on the environment variables
 if [ -r "${MQTT_CONFIG}/mosquitto.conf" ]
 then
-       	echo "* Copying configuration from ${MQTT_CONFIG}/mosquitto.conf ..."
+	echo "* Copying configuration from ${MQTT_CONFIG}/mosquitto.conf ..."
 	rm -rf /etc/mosquitto/*
 	cp "${MQTT_CONFIG}/mosquitto.conf" /etc/mosquitto/
 else
-       	echo "* Using env variables to generate configuration ..."
-       	touch "${MQTT_USERS_FILE}"
+	echo "* Using env variables to generate configuration ..."
+	touch "${MQTT_USERS_FILE}"
 	sed -ie "s|^log_dest .*\$|log_dest $MQTT_LOG|g" /etc/mosquitto/mosquitto.conf
 	sed -ie "s|^persistence .*\$|persistence $MQTT_PERSISTENCE|g" /etc/mosquitto/mosquitto.conf
 	sed -ie "s|^port .*\$|port $MQTT_PORT_DEFAULT|g" /etc/mosquitto/mosquitto.conf
@@ -84,7 +84,7 @@ else
 	if ! [ -z "${MQTT_USER}" ]
 	then
 		rm -f "${MQTT_USERS_FILE}"
-       		echo "* Generating user and password  ..."
+		echo "* Generating user and password  ..."
 		touch "${MQTT_USERS_FILE}"
 		mosquitto_passwd -b "${MQTT_USERS_FILE}" "${MQTT_USER}" "${MQTT_PASSWORD}"
 		sed -ie "s|^allow_anonymous .*\$|allow_anonymous false|g" /etc/mosquitto/mosquitto.conf
